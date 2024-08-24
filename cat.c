18ca352120319161ded42ff4884d7a9c14e4b667
Ahmed Hesham <ahmedhesham122000@gmail.com>
2024-08-24 19:54:45 +0300

    finsih step1: read file and write its content to stdoutput

--- cat.c
+++ cat.c
@@ -3,7 +3,21 @@
 #include<unistd.h>
 
 
-int main(int args, char* argv){
-   
+int main(int argc, char *argv[]){
+    FILE* fptr = fopen(argv[1], "r");
+    
+    if(fptr == NULL){
+        printf("Sorry, file not found\n");
+        printf("we will exit\n");
+        return -1; 
+    }
+
+    char fileContent[100];
+
+    // Read the content and print it
+    while(fgets(fileContent, 100, fptr)) {
+        printf("%s", fileContent);
+    }
+
     return 0; 
 }
